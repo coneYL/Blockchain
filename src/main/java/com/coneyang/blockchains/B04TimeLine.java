7803a5fb999627cfbe9c0a5a69eb1d5a40f4cb41
yanglei <yanglei@aixahc.com>
2020-07-16 10:14:12 +0800

    添加时间线，使可追溯可验证

---
+++ src/main/java/com/coneyang/blockchains/B04TimeLine.java
@@ -0,0 +1,93 @@
+/********************************************
+ * 功能说明: 
+ * 模块名称: 
+ * 系统名称: 
+ * 软件版权: 西安华信智慧数字科技有限公司
+ * 系统版本: 1.0.0
+ * 开发人员: zhangfb
+ * 开发时间: 2020/7/16 9:47
+ * 审核人员: 
+ * 相关文档: 
+ * 修改记录: 修改日期 修改人员 修改说明
+ *********************************************/
+package com.coneyang.blockchains;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * 通过加入时间，使区块链的时间只能向前，且可校验
+ * @author YangLei
+ * @version 1.0.0.1
+ * @since 2020/7/16 9:47
+ */
+public class B04TimeLine {
+    private static class Block{
+        public Block() {
+        }
+
+        public Block(String prev, String data, int nonce, Long time, String difficulty) {
+            this.prev = prev;
+            this.data = data;
+            this.nonce = nonce;
+            this.time = time;
+            this.difficulty = difficulty;
+        }
+
+        private String prev;
+        private String data;
+        private int nonce;
+        private Long time;
+        private String difficulty;
+
+        @Override
+        public String toString() {
+            //细节，nonce+time需要字符串拼接，不能数值相加
+            return this.nonce +""+ this.time + this.difficulty + prev + data;
+        }
+
+        public String getPrev() {
+            return prev;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public int getNonce() {
+            return nonce;
+        }
+
+        public Long getTime() {
+            return time;
+        }
+
+        public String getDifficulty() {
+            return difficulty;
+        }
+    }
+
+    public static String sha256(String content){
+        return DigestUtils.sha256Hex(content);
+    }
+
+    private static void printInfo(B04TimeLine.Block block){
+        System.out.println("#############");
+        System.out.println("data " + block.data);
+        System.out.println("difficulty " + block.difficulty);
+        System.out.println("nonce " + block.nonce);
+        System.out.println("prev " + block.prev);
+        System.out.println("time " + block.time);
+        System.out.println("#############");
+        System.out.println("");
+
+    }
+
+    public static void main(String[] args) {
+        B04TimeLine.Block first = new Block("0000000000000000000000000000000000000000000000000000000000000000","Hello, Cryptos!",215028,1521292321L,"0000");
+        String firstHash = sha256(first.toString());
+        System.out.println(firstHash);
+        printInfo(first);
+    }
+}
